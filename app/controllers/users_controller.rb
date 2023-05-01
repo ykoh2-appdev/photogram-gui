8d810cdd54535f705d4208c2cc5ac19a4ed4b7de
Yoon Koh <ykoh2@chicagobooth.edu>
2023-05-01 03:35:35 +0000

    user index

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+  def index
+    matching_users = User.all
+
+    @list_of_users = matching_users.order({ :username => :asc })
+
+    render({ :template => "user_templates/index.html.erb" })
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
 
+  get("/users", { :controller => "users", :action => "index" })
+
+  get("/users/")
 
 end
